477e5867adad4b0c0c7b13d0d4658e842c973028
Cyan <cyanisk@mailbox.org>
2022-06-01 00:21:40 +0200

    Mailing implemented

--- KombardoPinger.py
+++ KombardoPinger.py
@@ -1,20 +1,43 @@
+import smtplib
+import ssl
+import time
+import random
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import random
 
 #TODO: GUI
-#TODO: Booking times out after 20 minutes. Start anew when this happens
-#TODO: Notification stuff
-#TODO: Click elements when they are ready
+#TODO: Global timeout (to prevent running forever)
+#TODO: Take into consideration when a departure time is passed
 
+receiver = 'mads2112@live.dk'
 timeout = 20
 wait = 1
 auto_input = True
-input_list = ['Rønne-Ystad', 'Alm', '2', '30', '06:30', '16:30']
+input_list = ['Ystad-Rønne', 'Alm', '2', '3', '08:30', '18:30']
+
+def init_email(receiver):
+    mail_dict = {}
+    with open('variables.txt') as v:
+        for line in v:
+            (key, val) = line.split()
+            mail_dict[key] = val
+    
+    mail_dict['receiver'] = receiver
+    return mail_dict
+
+def send_mail(mail_dict, departure):
+    subject = 'Subject: Ledig billet [dato] kl. ' + departure + '\n\n'
+    text = 'Der er kommet ledige billetter til afgangen ' + input_list[0] + \
+        'd. ' + input_list[3] + '. kl. ' + departure
+    message = subject + text
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(mail_dict['smtp_server'],
+                          mail_dict['port'], context=context) as server:
+        server.login(mail_dict['sender'], mail_dict['password'])
+        server.sendmail(mail_dict['sender'], receiver, message)
 
 def wait_for_elem(driver, by, addr, timeout, wait):
     WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((by, addr)))
@@ -28,6 +51,12 @@ def auto_or_input(auto_input, auto):
     if auto_input:
         return auto
     return input()
+
+def is_timed_out():
+    return EC.element_to_be_clickable((By.XPATH, '//*[@id="js-booking"]/div/div[2]/div/button[2]'))
+
+# Get email info
+mail_dict = init_email(receiver)
 
 # Open website
 driver = webdriver.Firefox()
@@ -85,11 +114,6 @@ button_addr = '//*[@id="' + parent +'"]/' + button
 wait_for_elem(driver, By.ID, parent, timeout, wait)
 driver.find_element(By.XPATH, button_addr).click()
 
-#WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.ID, 'step-transport')))
-#rand_sleep(1)
-#driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li[2]/div/div[2]/ul/li[1]/button').click()
-#rand_sleep(4)
-
 # Trailer
 button = 'div/div[2]/ul/li[1]/button'
 parent = 'step-trailer'
@@ -97,11 +121,6 @@ button_addr = '//*[@id="' + parent +'"]/' + button
 
 wait_for_elem(driver, By.ID, parent, timeout, wait)
 driver.find_element(By.XPATH, button_addr).click()
-
-#WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.ID, 'step-trailer')))
-#rand_sleep(1)
-#driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li[1]/button').click()
-#rand_sleep(2)
 
 # Number of passengers
 print('Hvor mange passagerer? (1-9)')
@@ -113,12 +132,6 @@ button_addr = '//*[@id="' + parent +'"]/' + button
 
 wait_for_elem(driver, By.ID, parent, timeout, wait)
 driver.find_element(By.XPATH, button_addr).click()
-#num = int(input())
-
-#WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.ID, 'step-passengerCar')))
-#rand_sleep(1)
-#driver.find_element(By.XPATH, f'//*[@id="app-legacy"]/div/div/div/ul/li[2]/div/div[2]/div/div/ul/li[{num}]/label/span').click()
-#rand_sleep(5)
 
 # Date
 button = 'div/div[2]/button/div'
@@ -129,14 +142,12 @@ button_addr = '//*[@id="' + parent +'"]/' + button
 wait_for_elem(driver, By.XPATH, button_addr, timeout, wait)
 driver.find_element(By.XPATH, button_addr).click()
 
-#x_more_dates = '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/button/div'
-#WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, x_more_dates)))
-#rand_sleep(1)
-#driver.find_element(By.XPATH, x_more_dates).click()
-#rand_sleep(1)
-
-first = int(driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li[1]/button').text.split('\n')[1])
-last = int(driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li[28]/button').text.split('\n')[1])
+button = 'div/div[2]/ul/li[1]/button'
+button_addr = '//*[@id="' + parent +'"]/' + button
+first = int(driver.find_element(By.XPATH, button_addr).text.split('\n')[1])
+button = 'div/div[2]/ul/li[28]/button'
+button_addr = '//*[@id="' + parent +'"]/' + button
+last = int(driver.find_element(By.XPATH, button_addr).text.split('\n')[1])
 month_wrap = False
 month_last = 31
 if first > last:
@@ -148,16 +159,13 @@ if month_wrap:
 else:
     print('Vælg en dato mellem ' + str(first) + ' og ' + str(last))
 num = int(auto_or_input(auto_input, input_list[3]))
-#num = int(input())
+
 idx = (num - first + 1) % month_last
 
 button = f'div/div[2]/ul/li[{idx}]/button'
 button_addr = '//*[@id="' + parent +'"]/' + button
 
 driver.find_element(By.XPATH, button_addr).click()
-
-#driver.find_element(By.XPATH, f'//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li[{idx}]/button').click()
-#rand_sleep(5)
 
 # Time
 buttons = 'div/div[2]/ul/li'
@@ -166,9 +174,6 @@ buttons_addr = '//*[@id="' + parent +'"]/' + buttons
 
 wait_for_elem(driver, By.ID, parent, timeout, wait)
 
-#WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.ID, 'step-outbound/departure')))
-#rand_sleep(1)
-
 departures = driver.find_elements(By.XPATH, buttons_addr)
 dep_times = [dep.text.split('\n')[1] for dep in departures]
 print('Mulige afgange:')
@@ -176,10 +181,8 @@ for dep_time in dep_times:
     print(dep_time)
 
 print('Hvad er tidligste afganstidspunkt?')
-#first = dep_times.index(input())
 first = dep_times.index(auto_or_input(auto_input, input_list[4]))
 print('Hvad er seneste afgangstidspunkt?')
-#last = dep_times.index(input())
 last = dep_times.index(auto_or_input(auto_input, input_list[5]))
 
 available = [dep.text.split('\n')[-1] != 'UDSOLGT' for dep in departures]
@@ -194,23 +197,23 @@ for i in range(first, last+1):
 
 # Start looping
 #TODO: Implementer button/parent ting her
-webdriver.ActionChains(driver).move_to_element(departures[0]).perform()
 for i in range(20):
-    #/html/body/div[3]/div/div/div/div[2]/div
-    #/html/body/div[3]/div/div/div/div[2]/div/button[2]
+    #driver.find_element(By.XPATH, '//*[@id="js-booking"]/div/div[2]/div/button[2]').click()
     rand_sleep(300)
-    driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[1]/button').click()
-    #driver.back()
+    print('We gotta go back!')
+    #driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[1]/button').click()
+    driver.back()
     rand_sleep(5)
-    driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li[1]/button').click()
-    #driver.forward()
-    rand_sleep(5)
+    #driver.find_element(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li[1]/button').click()
+    driver.forward()
+    wait_for_elem(driver, By.ID, parent, timeout, 2)
     
-    new_dep = driver.find_elements(By.XPATH, '//*[@id="app-legacy"]/div/div/div/ul/li/div/div[2]/ul/li')
+    new_dep = driver.find_elements(By.XPATH, buttons_addr)
     new_avail = [dep.text.split('\n')[-1] != 'UDSOLGT' for dep in new_dep]
     
     for j in range(first, last+1):
-        if (available[j] | new_avail[j]) != available[j]:
+        if (not available[j]) and new_avail[j]:
+            send_mail(mail_dict, dep_times[j])
             print('Der er kommet ledige billetter til afgangen kl. ' + dep_times[j])
     
     departures = new_dep
